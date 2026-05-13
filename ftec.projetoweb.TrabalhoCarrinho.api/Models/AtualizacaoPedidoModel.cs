3658a89c2de6cde4ba1558534ef041899a9f87fa
DiogoTissian <diogotissianteixeira123@gmail.com>
2026-05-13 17:52:36 -0300

    ajuste

--- ftec.projetoweb.TrabalhoCarrinho.api/Models/AtualizacaoPedidoModel.cs
+++ ftec.projetoweb.TrabalhoCarrinho.api/Models/AtualizacaoPedidoModel.cs
@@ -6,13 +6,9 @@
         {
             this.PedidoId = Guid.Empty;
             this.StatusPedido = 0;
-            this.CEPEnderecoEntrega = string.Empty;
-            this.NumeroEnderecoEntrega = string.Empty;
         }
 
         public Guid PedidoId { get; set; }
         public int StatusPedido { get; set; }
-        public string CEPEnderecoEntrega { get; set; }
-        public string NumeroEnderecoEntrega { get; set; }
     }
 }
